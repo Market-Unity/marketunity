8449b80b759715d5fb5ab2926f4a1b8fc38b5d96
Phillip Schmidt <markidex@gmail.com>
2018-04-18 20:38:18 -0700

    Successful Persisting/Hashing of passwords.

--- data/db/helpers.js
+++ data/db/helpers.js
@@ -6,22 +6,25 @@ const mongoose = require('mongoose');
 // Register a new User
 const register = ({ username, password }, cb) => {
   let userTaken = true;
-  let hold = JSON.stringify(getUser(username, (err, data) => {
+  let hold = JSON.stringify(checkUser(username, (err, data) => {
     if (err) { console.log('Error in Mongo Find func', err); }
     userTaken = JSON.stringify(data) !== '[]';
     
     if (userTaken === false) {
-      let createUser = new newUser({
-        username: username,
-        password: password
-      });
-
-      createUser.save((err, data) => {
-        if (err) {
-          console.log('There was a DB insertion error: ', err);
-          cb(err, null);
-        }
-        cb(null, data);
+      // Hashing Password Here
+      hashPass(password, (err, hash) => {
+        let createUser = new newUser({
+          username: username,
+          password: hash
+        });
+  
+        createUser.save((err, data) => {
+          if (err) {
+            console.log('There was a DB insertion error: ', err);
+            cb(err, null);
+          }
+          cb(null, data);
+        });
       });
     } else {
       err = 'Username taken!';
@@ -31,7 +34,7 @@ const register = ({ username, password }, cb) => {
 };
 
 // Check if a username is already taken
-const getUser = (username, cb) => {
+const checkUser = (username, cb) => {
   newUser.find({
     username: username
   }, (err, data) => {
@@ -40,7 +43,17 @@ const getUser = (username, cb) => {
   });
 };
 
+const hashPass = (password, cb) => {
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, null, (err, hash) => {
+      if (err) { cb(err, null); }
+      cb(null, hash);
+    });
+  });
+};
+
 
 module.exports = {
-  register
+  register,
+  checkUser
 };
