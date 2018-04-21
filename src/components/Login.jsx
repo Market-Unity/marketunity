7b9706cf45a2d605de8871bb2be7aa1f499cbf01
Eric Park <erichoonpark@gmail.com>
2018-04-21 10:45:19 -0700

    updated components

---
+++ src/components/Login.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import "./css/Login.css"
+import axios from 'axios';
+
+export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username : '',
+      password : ''
+    }
+  }
+
+  //Store the password & username upon input value change
+  handleChange = (event) => {
+    this.setState({
+      [event.target.id] : event.target.value
+    });
+  }
+
+  //Submit the login credentials to the server for verification
+  handleSubmit = (un, pw) => {
+    //POST Request to login route, using axios
+    console.log(un, pw);
+    // axios.post('/login', {username : this.state.username, password : this.state.password}).then(function() {
+    //
+    // })
+  }
+
+  //Render the form
+  render() {
+    return (
+      <Form onSubmit = {this.handleSubmit(this.state.username, this.state.password)}>
+        <FormGroup>
+          <Label for="exampleEmail">Email</Label>
+          <Input type="email" name="email" id="username" placeholder="Email" onChange={this.handleChange}/>
+        </FormGroup>
+        <FormGroup>
+          <Label for="examplePassword">Password</Label>
+          <Input type="password" name="password" id="password" placeholder="Password" onChange={this.handleChange}/>
+        </FormGroup>
+        <Button>Submit</Button>
+      </Form>
+    );
+  }
+}
---
+++ src/components/Search.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import "./css/Search.css"
+
+export default class Search extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Form inline>
+        <FormGroup>
+          <Label for="search" hidden>Search</Label>
+          <Input type="search" name="search" id="search" placeholder="search" onChange = {this.props.handleQuery}/>
+        </FormGroup>
+        {' '}
+        <Button>Submit</Button>
+      </Form>
+    );
+  }
+}
