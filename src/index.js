97ec16f05ec39716373cc8171586b63eb65b1418
Amogh Kambale <me@amoghk.com>
2018-04-26 10:34:40 -0700

    App is rendering

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
-, document.getElementById('root'));
+  , document.getElementById('app'));
 registerServiceWorker();
