ab23e711d53cc6e8300781deca095066b09698e6
Eric Park <erichoonpark@gmail.com>
2018-04-27 11:41:25 -0700

    Completed routes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import {
   NavItem,
   NavLink
 } from 'reactstrap';
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './css/Navbar.css';
 
 export default class NavigationBar extends React.Component {
@@ -18,20 +18,20 @@ export default class NavigationBar extends React.Component {
     return (
       <div>
         <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">
+          <NavbarBrand tag={Link} to="/">
             <img src="https://image.ibb.co/cvsaP7/logo.png" className="nav-logo" alt="Armadillo"></img>Market-Unity
           </NavbarBrand>
             <Nav className="mr-auto" navbar>
               <NavItem>
-                <NavLink href="/favorites/">Favorites</NavLink>
+                <NavLink tag={Link} to="/favorites">Favorites</NavLink>
               </NavItem>
             </Nav>
             <Nav className="ml-auto" navbar>
               <NavItem>
-                <NavLink href="/login/">Login</NavLink>
+                <NavLink tag={Link} to="/login">Login</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="/signup/">Sign Up</NavLink>
+                <NavLink tag={Link} to="/signup">Sign Up</NavLink>
               </NavItem>
             </Nav>
         </Navbar>
